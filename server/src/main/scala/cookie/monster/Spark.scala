7d18552e81af53145dac4356e563dbabc4937936
Alan <alanmf@gmail.com>
2016-09-06 01:16:53 -0600

    Added some commenting and new function

--- server/src/main/scala/cookie/monster/Spark.scala
+++ server/src/main/scala/cookie/monster/Spark.scala
@@ -28,21 +28,62 @@ object SparkReporting {
     }
   })
 
+  /*
+  Return the URL of the host domain that is responsible for setting the most cookies.
+
+  Sample call:
+
+  scala> val x = cookie.monster.SparkReporting.mostCommonUrlHost
+  x: Option[String] = Some(www.facebook.com)
+  */
   def mostCommonUrlHost(): Option[String] = {
     result.map { r =>
       Uri.parse(r.url).host
     }.countByValue().maxBy(_._2)._1
   }
 
-  def sitesWithMostCookies() {
+  /*
+  Get the pages that are setting the most cookies.
 
+  Sample call:
+
+  scala> val x = cookie.monster.SparkReporting.pagesWithMostCookies
+  x: Seq[(String, Int)] = 
+  WrappedArray(
+  (http://usuncut.com,96),
+  (http://wnd.com,87),
+  (http://wnd.com,85),
+  (http://photobucket.com,85),
+  (http://qpolitical.com,85),
+  (http://readysethealth.com,85),
+  (http://theblaze.com,85)
+  )
+  */
+  def pagesWithMostCookies(): Seq[(String, Int)] = {
+    result.map { r => 
+      (
+        r.pageref, 
+        r.responseCookies.split(";").size
+      )
+    }.takeOrdered(10)(Ordering[Int].reverse.on{x => x._2 }).toSeq
   }
 
-  def hostsSettingMostCookies() {
-    
-  }
+  /* 
+  Return a random sample of keys with their grouped values.
 
-  //Return a random sample of keys with their grouped values.
+  Sample return:
+
+  scala> val x = cookie.monster.SparkReporting.cookieKeysWithValues(5)
+
+  x: Seq[(String, Iterable[String])] =
+  WrappedArray(
+    ("mapp",CompactBuffer(0)),
+    (" HttpOnly_mkra_ctxt",CompactBuffer(ac7bccdbc00ca477b1cf8b061b618f01--200)),
+    (" HttpOnlyW2A",CompactBuffer(!HUQWu94cV0pavwFLTQ+Tjs7s3Sheluk2E8lYgeb7YGLFKwk/WAyKF9vflv+ypVwUJ6+uk61cPEdSXQ)),
+    (visid_incap_851464,CompactBuffer(Wwk2EWA2TjGjTPPKE+8mbc7FzVcAAAAAQUIPAAAAAADSPOloT3zk80jAxGCrARff)),
+    (_access_token,CompactBuffer(, , , , , , , ))
+  )
+  */
   def cookieKeysWithValues(sampleSize: Int): Seq[(String, Iterable[String])] = {
     result.flatMap { r =>
       val pairs = r.responseCookies.split(";")
